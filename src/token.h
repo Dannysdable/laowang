54285a588e805981483d745b0178f1e2d07864cb
AndyQu <andy@AndyQudeMacBook-Pro.local>
2017-12-27 21:25:29 +0800

    增加关键字，能正常解析a.b这类的字段或表名+字段，还有between and和join on/in这类不知道怎么加

--- src/token.h
+++ src/token.h
@@ -12,7 +12,14 @@ typedef enum {
     T_Is = 256, T_Le, T_Ge, T_Eq, T_Ne, T_And, T_Or, T_IntConstant,
     T_StringConstant, T_Identifier, T_Void, T_Int, T_While,
     T_If, T_Else, T_Return, T_Break, 
-    T_Select, T_From, T_Where, T_Content, T_Value
+    T_Select, T_From, T_Where, T_Decode, T_Kand, T_Union_all, T_Order_by,
+	T_Group_by, T_Rownum, T_Desc, 
+	/* 内部函数 */
+	T_Sum, T_Min, T_Trunc, T_Max, T_Nvl2, T_Nvl, T_Length, T_Nullif, T_Coalesce, T_Having,
+	T_To_char, T_Case, T_End, T_Avg,
+	T_When, T_Then, T_Using,
+	T_Point, T_StringConstant2,
+	T_Content, T_Value
 } TokenType;
 
 static void print_token(int token) {
@@ -20,7 +27,14 @@ static void print_token(int token) {
         "T_Is", "T_Le", "T_Ge", "T_Eq", "T_Ne", "T_And", "T_Or", "T_IntConstant",
         "T_StringConstant", "T_Identifier", "T_Void", "T_Int", "T_While",
         "T_If", "T_Else", "T_Return", "T_Break", 
-        "T_Select", "T_From", "T_Where", "T_Content", "T_Value"
+        "T_Select", "T_From", "T_Where", "T_Decode", "T_Kand", 
+		"T_Union_all", "T_Order_by", "T_Group_by", "T_Rownum", "T_Desc", 
+		/* 内部函数 */
+		"T_Sum", "T_Min", "T_Trunc", "T_Max", "T_Nvl2", "T_nvl", "T_Length", "T_Nullif", "T_Coalesce", "T_Having",
+		"T_To_char", "T_Case", "T_End", "T_Avg",
+		"T_When", "T_Then", "T_Using",
+		"T_Point", "T_StringConstant2",
+		"T_Content", "T_Value"
     };
 
     if (token < 256) {
